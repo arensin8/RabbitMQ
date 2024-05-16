aebab155d5d6a33bfafce3e3801041695db0f5d0
arensin8 <arensinani4@gmail.com>
2024-05-16 18:48:45 +0400

    adding configs

--- consumer.js
+++ consumer.js
@@ -2,19 +2,29 @@ const amqp = require("amqplib");
 
 async function receiveFromService1() {
   const queueName = "task";
-  const connection = await amqp.connect("amqp://localhost:5672");
-  const chanel = connection.createChannel();
-  let index = 0;
-  (await chanel).assertQueue(queueName, { durable: true });
-  (await chanel).consume(
-    queueName,
-    (msg) => {
-      console.log(`${index}:`, msg.content.toString());
-      index++;
-    },
-    //it means consumer confirmes messages
-    { noAck: true }
-  );
+
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queueName, { durable: true });
+    let index = 0;
+    await channel.consume(
+      queueName,
+      (msg) => {
+        setTimeout(() => {
+          console.log(`${index}:`, msg.content.toString());
+          index++;
+          // Acknowledge the message manually
+          channel.ack(msg);
+        }, 2000);
+      }
+      // { noAck: true } // Uncomment this line if you do not want to acknowledge messages manually
+    );
+
+    console.log("Waiting for messages in %s. To exit press CTRL+C", queueName);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 receiveFromService1();
--- producer.js
+++ producer.js
@@ -6,21 +6,23 @@ async function sendMsgToTask() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
-    channel.sendToQueue(queueName, Buffer.from("Hello RabbitMQ"), 
-    //this is for saving data on disk if sth happens (restart rabbitmq , ...)
-    {
-      persistent: true,
-    });
+    channel.sendToQueue(
+      queueName,
+      Buffer.from("Hello RabbitMQ"),
+      //this is for saving data on disk if sth happens (restart rabbitmq , ...)
+      {
+        persistent: true,
+      }
+    );
     console.log("Message sent to service");
     setTimeout(() => {
       connection.close();
-      process.exit(0);
     }, 1000);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-for (let index = 0; index < 20; index++) {
+setInterval(() => {
   sendMsgToTask();
-}
+}, 1000);
